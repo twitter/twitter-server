bb9780e84c695485b852dd93a166f6535b673bce
marius a. eriksen <marius@twitter.com>
2013-09-04 20:50:01 +0000

    [split] Introduce swappable schedulers, ThreadPool scheduler.
    RB_ID=176993

--- src/main/scala/com/twitter/server/util/TwitterStats.scala
+++ src/main/scala/com/twitter/server/util/TwitterStats.scala
@@ -9,20 +9,14 @@ object TwitterStats {
     val sched = statsReceiver.scope("scheduler")
 
     sched.addGauge("productivity") {
-      var cpu, usr = 0L
-      for (s <- Scheduler.schedulers) {
-        cpu += s.cpuTime
-        usr += s.usrTime
-      }
+      val cpu = Scheduler.cpuTime
+      val usr = Scheduler.usrTime
       if (cpu.toFloat == 0F) 0F
       else usr.toFloat / cpu.toFloat
     }
     
     sched.addGauge("dispatches") {
-      var n = 0L
-      for (s <- Scheduler.schedulers)
-        n += s.numDispatches
-      n.toFloat
+      Scheduler.numDispatches.toFloat
     }
   }
 }
