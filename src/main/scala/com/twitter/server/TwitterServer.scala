20d07e4976e53a2c9c27da0a0039f4d4190b7e40
Moses Nakamura <mnakamura@twitter.com>
2013-09-17 13:56:40 -0700

    [split] Revert "twitter-server: Add a Closer trait, which provides graceful shutdown of `Closable`s."
    This reverts commit 4d60ddb2ad23ce89e6219d300a25f266921c56a1.

--- src/main/scala/com/twitter/server/TwitterServer.scala
+++ src/main/scala/com/twitter/server/TwitterServer.scala
@@ -2,6 +2,7 @@ package com.twitter.server
 
 import com.twitter.app.App
 import com.twitter.logging.Logging
+import com.twitter.finagle.http.HttpMuxer
 
 trait TwitterServer extends App
   with AdminHttpServer
