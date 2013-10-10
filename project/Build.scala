f1bfd5bd012225d3b3c2212b06e7db92693aac8a
Evan Meagher <evm@twitter.com>
2013-10-10 18:07:03 +0000

    [split] release finagle 6.7.1
    The release of Finagle 6.7.0 failed midway through due to tooling
    issues [1], leaving artifacts published for some subprojects but not
    others. We're rolling forward with a 6.7.1 release.
    
    [1] https://reviewboard.twitter.biz/r/205208/[INFO] finagle-benchmark
    
    RB_ID=205426
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object TwitterServer extends Build {
   val libVersion = "1.1.0"
   val utilVersion = "6.6.0"
-  val finagleVersion = "6.6.2"
+  val finagleVersion = "6.7.1"
 
   def util(which: String) = "com.twitter" %% ("util-"+which) % utilVersion
   def finagle(which: String) = "com.twitter" %% ("finagle-"+which) % finagleVersion
