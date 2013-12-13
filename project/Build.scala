d89f018dfe433580d7b99486e75f3c901e89cebd
Brian Degenhardt <bdegenhardt@twitter.com>
2013-12-13 21:56:14 +0000

    [split] release 1.4.0 of twitter-server
    RB_ID=253533
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.3.1"
+  val libVersion = "1.4.0"
   val utilVersion = "6.10.0"
   val finagleVersion = "6.10.0"
   val jacksonVersion = "2.2.2"
