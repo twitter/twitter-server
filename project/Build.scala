90cce532106e64e4b850f193a75909d35bd39150
Moses Nakamura <mnakamura@twitter.com>
2014-01-22 22:13:33 +0000

    [split] bump twitter-server to 1.4.2-SNAPSHOT
    RB_ID=271155
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.4.0"
+  val libVersion = "1.4.1"
   val utilVersion = "6.11.1"
   val finagleVersion = "6.11.1"
   val jacksonVersion = "2.2.2"
