6d221bd5ae9d519c44ff55bcc802a57dd2b28340
Steve Gury <stevegury@twitter.com>
2014-02-15 07:21:31 +0000

    [split] release twitter-server version 1.5.0
    RB_ID=289281
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.4.1"
+  val libVersion = "1.5.0"
   val utilVersion = "6.12.0"
   val finagleVersion = "6.12.0"
   val jacksonVersion = "2.2.2"
