3e64777690aa8ec2e99c3be1f0b3fe55bfe74b1b
Steve Gury <stevegury@twitter.com>
2014-02-19 03:56:53 +0000

    [split] release finable version 6.12.1
    RB_ID=290583
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object TwitterServer extends Build {
   val libVersion = "1.5.0"
   val utilVersion = "6.12.1"
-  val finagleVersion = "6.12.0"
+  val finagleVersion = "6.12.1"
   val jacksonVersion = "2.2.2"
 
   val jacksonLibs = Seq(
