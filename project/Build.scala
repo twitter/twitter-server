20f22212bc899691dc17db8aa2c76c26381d3c14
Steve Gury <stevegury@twitter.com>
2014-03-24 13:48:59 -0700

    [split] release finagle version 6.13.1
    RB_ID=317383
    TBR=true
    NO-QUEUE=true

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,7 @@ import com.typesafe.sbt.site.SphinxSupport.Sphinx
 object TwitterServer extends Build {
   val libVersion = "1.6.0"
   val utilVersion = "6.13.2"
-  val finagleVersion = "6.13.0"
+  val finagleVersion = "6.13.1"
   val jacksonVersion = "2.2.2"
 
   val jacksonLibs = Seq(
