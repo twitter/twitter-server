09bfeab5f54d9f80b049a22a606bd4345a9bf040
Moses Nakamura <mnakamura@twitter.com>
2014-08-06 00:54:29 +0000

    [split] bump all the things
    RB_ID=422405
    TBR=true

--- project/Build.scala
+++ project/Build.scala
@@ -5,9 +5,9 @@ import com.typesafe.sbt.SbtSite.site
 import com.typesafe.sbt.site.SphinxSupport.Sphinx
 
 object TwitterServer extends Build {
-  val libVersion = "1.7.2"
-  val utilVersion = "6.18.2"
-  val finagleVersion = "6.19.0"
+  val libVersion = "1.7.3"
+  val utilVersion = "6.18.3"
+  val finagleVersion = "6.20.0"
   val jacksonVersion = "2.3.1"
   val mustacheVersion = "0.8.12.1"
 
