b13536208c7dafb74bfdcda11df0de924a0b4290
marius a. eriksen <marius@twitter.com>
2014-09-11 21:02:19 +0000

    [split] finagle: simplify Stack clients and servers. Unify around them.
    We introduce StdStack{Client,Server} and apply them consistently
    across clients and servers. This simplifies our current "new style"
    protocol implementations and makes them more flexible. Users can now
    customize, parameterize, and otherwise alter the entirety of the
    finagle stack through a few, carefully selected primitives. (Namely,
    Stack, Stack.Params, and StdStack*.) Client and servers are now
    first-class values that may be manipulated through the use of these
    primitives.
    
    This makes consistent use of Stack for our various clients and
    servers. Old uses in package exp are removed.
    
    RB_ID=451549

--- src/main/scala/com/twitter/server/AdminHttpServer.scala
+++ src/main/scala/com/twitter/server/AdminHttpServer.scala
@@ -1,11 +1,11 @@
 package com.twitter.server
 
 import com.twitter.app.App
-import com.twitter.finagle.http.{Http, HttpMuxer}
+import com.twitter.finagle.http.HttpMuxer
 import com.twitter.finagle.netty3.Netty3Listener
 import com.twitter.finagle.stats.NullStatsReceiver
 import com.twitter.finagle.tracing.NullTracer
-import com.twitter.finagle.{ServerCodecConfig, NullServer, ListeningServer, HttpServer}
+import com.twitter.finagle.{Http, ListeningServer, NullServer, param}
 import com.twitter.util.Monitor
 import java.net.{SocketAddress, InetSocketAddress}
 import java.util.logging.Logger
@@ -13,18 +13,10 @@ import java.util.logging.Logger
 trait AdminHttpServer { self: App =>
   def defaultHttpPort = 9990
   val adminPort = flag("admin.port", new InetSocketAddress(defaultHttpPort), "Admin http server port")
-
+  
   @volatile protected var adminHttpServer: ListeningServer = NullServer
 
   premain {
-    // Use NullStatsReceiver to keep admin endpoints from getting their stats mixed in
-    // with the service's stats
-    // TODO: use StackServer when available
-    val httpListener = Netty3Listener[Any, Any]("http",
-      Http().server(ServerCodecConfig("httpserver", new SocketAddress{})).pipelineFactory,
-      statsReceiver = NullStatsReceiver
-    )
-
     val log = Logger.getLogger(getClass.getName)
     val loggingMonitor = new Monitor {
       def handle(exc: Throwable): Boolean = {
@@ -33,11 +25,10 @@ trait AdminHttpServer { self: App =>
       }
     }
 
-    adminHttpServer = HttpServer
-      .copy(listener = httpListener)
-      .copy(statsReceiver = NullStatsReceiver)
-      .copy(tracer = NullTracer)
-      .copy(monitor = loggingMonitor)
+    adminHttpServer = Http.server
+      .configured(param.Stats(NullStatsReceiver))
+      .configured(param.Tracer(NullTracer))
+      .configured(param.Monitor(loggingMonitor))
       .serve(adminPort(), HttpMuxer)
   }
 
