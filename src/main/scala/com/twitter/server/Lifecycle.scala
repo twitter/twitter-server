79522718e1df6e38da2daf75fbd9fda25ab7cdb2
Kevin Oliver <koliver@twitter.com>
2014-10-07 04:50:59 +0000

    [split] twitter-server: promote objects to old gen before serving
    Problem
    
    Early in a service's lifecycle, garbage collection times take longer
    than those during it's steady state. Some of this is due to objects
    with long lifetimes being created and then having to get copied back
    and forth between the survivor spaces before finally getting tenured
    into the old generation.
    
    Solution
    
    Add a flag `com.twitter.server.promoteBeforeServing` that allows
    services to opt into having objects promoted to the old generation
    before serving traffic.
    
    Result
    
    Shorter garbage collection pauses while the service is taking traffic.
    
    RB_ID=473845

--- src/main/scala/com/twitter/server/Lifecycle.scala
+++ src/main/scala/com/twitter/server/Lifecycle.scala
@@ -1,8 +1,11 @@
 package com.twitter.server
 
-import com.twitter.app.App
+import com.twitter.app.{GlobalFlag, App}
 import com.twitter.finagle.http.HttpMuxer
 import com.twitter.server.handler._
+import java.lang.management.ManagementFactory
+import java.util.concurrent.atomic.AtomicBoolean
+import scala.collection.JavaConverters._
 
 trait Lifecycle { self: App =>
   // Mesos/Aurora lifecycle endpoints
@@ -11,7 +14,48 @@ trait Lifecycle { self: App =>
   HttpMuxer.addHandler("/health", new ReplyHandler("OK\n"))
 }
 
+object promoteBeforeServing extends GlobalFlag[Boolean](false,
+  "Promote objects in young generation to old generation before serving requests. " +
+    "May shorten the following gc pauses by avoiding the copying back and forth between survivor " +
+    "spaces of a service's long lived objects.")
+
 object Lifecycle {
+
+  private[server] class PromoteToOldGen(
+      runtimeArgs: Seq[String] = ManagementFactory.getRuntimeMXBean.getInputArguments.asScala)
+  {
+    private[this] val hasPromoted = new AtomicBoolean(false)
+
+    private[server] def promoted: Boolean = hasPromoted.get()
+
+    /**
+     * Depending on the VM flags used, `System.gc` will not have the effect we want.
+     */
+    private[this] def shouldExplicitGc: Boolean = {
+      !runtimeArgs.contains("-XX:+ExplicitGCInvokesConcurrent")
+    }
+
+    /**
+     * Helps make early gc pauses more consistent.
+     *
+     * Ideally this runs in the moments right before your service starts accepting traffic.
+     *
+     * Note: while we gather more data points, you must explicitly set the
+     * `promoteBeforeServing` flag to true, but we expect to default that to true in the future.
+     * (the application flags must have already been parsed).
+     */
+    def beforeServing(): Unit = {
+      if (promoteBeforeServing() && shouldExplicitGc && hasPromoted.compareAndSet(false, true)) {
+        // This relies on the side-effect of a full gc that all objects in the
+        // young generation are promoted to the old generation.
+        // If this side-effect were to disappear in a future version
+        // of the jdk, it would not be disastrous. However, then we may choose
+        // to add a `System.promoteAllLiveObjects()` hook.
+        System.gc()
+      }
+    }
+  }
+
   /**
    * Give the application control over when to present to Mesos as being ready
    * for traffic. When the method `warmupComplete()` is invoked, the application
@@ -19,6 +63,10 @@ object Lifecycle {
    */
   trait Warmup {
     HttpMuxer.addHandler("/health", new ReplyHandler(""))
-    def warmupComplete() = HttpMuxer.addHandler("/health", new ReplyHandler("OK\n"))
+
+    def warmupComplete(): Unit = {
+      new PromoteToOldGen().beforeServing()
+      HttpMuxer.addHandler("/health", new ReplyHandler("OK\n"))
+    }
   }
 }
