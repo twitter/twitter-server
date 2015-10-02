5a3e432d4f3d8ccfe40e513e04fc4b9e5cb6b5d9
Ruben Oanta <roanta@twitter.com>
2015-10-02 21:13:29 +0000

    util-core: decommission scheduler clock stats
    The Scheduler clock stats were decommissioned as they only make
    sense relative to `wallTime` and the tracking error we have experienced
    in production between `wallTime` and `*Time` make it impossible to use
    them reliably. It is not worth the performance and code complexity
    to support them.
    
    RB_ID=750239

--- src/main/scala/com/twitter/server/util/TwitterStats.scala
+++ src/main/scala/com/twitter/server/util/TwitterStats.scala
@@ -8,17 +8,6 @@ object TwitterStats {
   def register(statsReceiver: StatsReceiver): Unit = {
     val scheduler = statsReceiver.scope("scheduler")
 
-    // Productivity is a very rough estimate of time spent not
-    // blocking. It measures the proportion of a thread's execution
-    // time spent on the CPU. This cannot take into account issues
-    // like CPU scheduling effects.
-    scheduler.provideGauge("productivity") {
-      val cpu = Scheduler.cpuTime
-      val wall = Scheduler.wallTime
-      if (wall.toFloat <= 0F) 0F
-      else cpu.toFloat / wall.toFloat
-    }
-
     scheduler.provideGauge("dispatches") {
       Scheduler.numDispatches.toFloat
     }
