1383d183a1c1788f1df26e9989feaa7e02ed5134
Ryan O'Neill <ryano@twitter.com>
2015-11-20 23:17:58 +0000

    twitter-server: Change AdminHttpServer Tests to Properly Execute
    Problem
    
    The tests which involve checking whether the AdminServer and the
    ShadowAdminServer are correctly serving stats and then closing are
    not being run properly. The created TwitterServer instances do not
    have their main method being called.
    
    Solution
    
    Create the individual servers and then call the main method of each
    instance to ensure that the checkServer method gets run for each test.
    Also, the ShadowAdminServer does not listen for the /quitquitquit
    message so send that message instead to the AdminServer
    
    Result
    
    The tests are being properly executed and now are passing.
    
    RB_ID=770891

--- src/test/scala/com/twitter/server/AdminHttpServerTest.scala
+++ src/test/scala/com/twitter/server/AdminHttpServerTest.scala
@@ -1,12 +1,13 @@
 package com.twitter.server
 
 import com.twitter.app.App
+import com.twitter.conversions.time._
 import com.twitter.finagle.Http
 import com.twitter.finagle.http.{HttpMuxHandler, Request, Response}
 import com.twitter.finagle.ListeningServer
 import com.twitter.server.util.HttpUtils._
 import com.twitter.util.{Duration, Await, Future}
-import java.net.InetSocketAddress
+import java.net.{InetAddress, InetSocketAddress}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.concurrent.Eventually
@@ -35,35 +36,54 @@ class MockHostMetricsExporter extends HttpMuxHandler {
 @RunWith(classOf[JUnitRunner])
 class AdminHttpServerTest extends FunSuite  {
 
-  def checkServer(app: App, server: ListeningServer): Unit = {
+  def checkServer(server: ListeningServer): Unit = {
     val port = server.boundAddress.asInstanceOf[InetSocketAddress].getPort
     val client = Http.client.newService(s"localhost:$port")
 
-    val resp0 = Await.result(client(Request("/stats.json")))
+    val resp0 = Await.result(client(Request("/stats.json")), 1.second)
     assert(resp0.contentString.contains("metrics!"))
 
-    val resp = Await.result(client(Request("/admin/metrics.json")))
+    val resp = Await.result(client(Request("/admin/metrics.json")), 1.second)
     assert(resp.contentString.contains("standard metrics!"))
 
-    val resp1 = Await.result(client(Request("/admin/per_host_metrics.json")))
+    val resp1 = Await.result(client(Request("/admin/per_host_metrics.json")), 1.second)
     assert(resp1.contentString.contains("per host metrics!"))
+  }
 
-    Await.result(client(Request("/quitquitquit")))
+  def closeServer(app: App, server: ListeningServer): Unit = {
+    val port = server.boundAddress.asInstanceOf[InetSocketAddress].getPort
+    val client = Http.client.newService(s"localhost:$port")
+
+    Await.result(client(Request("/quitquitquit")), 1.second)
 
     // throws if adminHttpServer does not exit before the grace period,
     // which indicates that we have not closed it properly.
-    Await.result(server, app.defaultCloseGracePeriod)
+    Await.result(server, 2.seconds)
   }
 
-  test("server serves and is closed properly") (new TestTwitterServer {
-    override def main() {
-      checkServer(this, adminHttpServer)
+  test("server serves and is closed properly") {
+    val server = new TestTwitterServer {
+      override def main() {
+        checkServer(adminHttpServer)
+        closeServer(this, adminHttpServer)
+        Await.result(close(5.seconds))
+      }
     }
-  })
+    server.main(args = Array.empty[String])
+  }
 
-  test("shadow server serves and is closed properly")(new TestTwitterServer with ShadowAdminServer {
-    override def main() {
-      checkServer(this, shadowHttpServer)
+  test("shadow server serves and is closed properly") {
+    val server = new TestTwitterServer with ShadowAdminServer {
+      override def main() {
+        checkServer(shadowHttpServer)
+        // ShadowAdminServer does not listen for /quitquitquit
+        // so send it to the admin server
+        closeServer(this, adminHttpServer)
+        Await.result(close(5.seconds))
+      }
     }
-  })
+    server.shadowAdminPort.let(new InetSocketAddress(InetAddress.getLoopbackAddress, 0)) {
+      server.main(args = Array.empty[String])
+    }
+  }
 }
