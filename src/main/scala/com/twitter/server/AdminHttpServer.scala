daebff2cadaf3ac94c3d9c5561b3bf73eb8138d1
Bing Wei <bing@twitter.com>
2016-01-10 19:13:18 +0000

    finagle-core: introduce Admission controller in the server stack
    Problem
    
    The Finagle server does not have a way to dynamically
    reject requests when it's overloaded. It can go into
    failure spiral without a way to recover until it's
    restarted.
    
    Solution
    
    Introduce the `c.t.f.filter.ServerAdmissionControl` module
    in the server Stack, which is enabled through param
    `c.t.f.param.EnableServerAdmissionControl`. There are can be
    multiple implementations of admission control filters
    which are registered through `ServerAdmissionControl.register`
    method. It's up to each AC filter to define their own
    way of detecting server over capacity and configuration.
    
    Server admission control is on by default in the server
    Stack except for TwitterServer admin server.
    
    Result
    
    Provide users a way to define their own admission control
    logic, and hook it up in the server Stack.
    
    RB_ID=776385

--- src/main/scala/com/twitter/server/AdminHttpServer.scala
+++ src/main/scala/com/twitter/server/AdminHttpServer.scala
@@ -1,12 +1,13 @@
 package com.twitter.server
 
 import com.twitter.app.App
+import com.twitter.finagle.{Http, ListeningServer, NullServer, Service, param}
 import com.twitter.finagle.client.ClientRegistry
-import com.twitter.finagle.http.{Response, Request, HttpMuxer}
+import com.twitter.finagle.filter.ServerAdmissionControl
+import com.twitter.finagle.http.{HttpMuxer, Request, Response}
 import com.twitter.finagle.server.ServerRegistry
 import com.twitter.finagle.stats.NullStatsReceiver
 import com.twitter.finagle.tracing.NullTracer
-import com.twitter.finagle.{Http, ListeningServer, NullServer, param, Service}
 import com.twitter.server.util.HttpUtils
 import com.twitter.server.view.{IndexView, NotFoundView}
 import com.twitter.util.{Future, Monitor}
@@ -187,6 +188,9 @@ trait AdminHttpServer { self: App =>
       .configured(param.Tracer(NullTracer))
       .configured(param.Monitor(loggingMonitor))
       .configured(param.Label(ServerName))
+      // disable admission control, since we want the server to report stats
+      // especially when it's in a bad state.
+      .configured(ServerAdmissionControl.Param(false))
       .serve(adminPort(), new NotFoundView andThen adminHttpMuxer)
 
     closeOnExit(adminHttpServer)
