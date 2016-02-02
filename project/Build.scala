7e24f2713ca67ace47d5c27f315838bf7a8aad08
Christopher Coco <ccoco@twitter.com>
2016-02-02 21:18:24 +0000

    Prepare for open-source release of CSL libraries
    RB_ID=796003

--- project/Build.scala
+++ project/Build.scala
@@ -9,9 +9,9 @@ object TwitterServer extends Build {
   val branch = Process("git" :: "rev-parse" :: "--abbrev-ref" :: "HEAD" :: Nil).!!.trim
   val suffix = if (branch == "master") "" else "-SNAPSHOT"
 
-  val libVersion = "1.16.0" + suffix
-  val utilVersion = "6.30.0" + suffix
-  val finagleVersion = "6.31.0" + suffix
+  val libVersion = "1.17.0" + suffix
+  val utilVersion = "6.31.0" + suffix
+  val finagleVersion = "6.32.0" + suffix
   val mustacheVersion = "0.8.18"
 
   val jacksonVersion = "2.4.4"
