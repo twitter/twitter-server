562f50a250c7e0f5d81d38f3d15786ce64113fa0
Jake Donham <jdonham@twitter.com>
2016-10-20 00:42:33 +0000

    twitter-server: don't retain JsonConverter buffer in a ThreadLocal
    RB_ID=882822

--- src/main/scala/com/twitter/server/util/JsonConverter.scala
+++ src/main/scala/com/twitter/server/util/JsonConverter.scala
@@ -1,14 +1,17 @@
 package com.twitter.server.util
 
+import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.core.util.{DefaultIndenter, DefaultPrettyPrinter}
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{MappingJsonFactory, ObjectMapper}
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.twitter.finagle.http.{Response, Status, Version}
 import com.twitter.io.Buf
 
 object JsonConverter {
   private[this] val writer = {
-    val mapper = new ObjectMapper().registerModule(DefaultScalaModule)
+    val factory = new MappingJsonFactory()
+    factory.disable(JsonFactory.Feature.USE_THREAD_LOCAL_FOR_BUFFER_RECYCLING)
+    val mapper = new ObjectMapper(factory).registerModule(DefaultScalaModule)
     val printer = new DefaultPrettyPrinter
     printer.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE)
     mapper.writer(printer)
