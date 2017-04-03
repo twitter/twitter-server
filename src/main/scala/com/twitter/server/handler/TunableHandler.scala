dc78831573896f17a2bd386ce3338dde70d194ec
Jillian Crossley <jillianc@twitter.com>
2017-04-03 15:53:12 +0000

    twitter-server, util-tunable: Endpoint to clear Tunables
    Problem
    
    We'd like to be able to clear in-memory Tunables via
    an endpoint in Twitter Server admin.
    
    Solution
    
    Handle DELETE requests to the /admin/tunables/ endpoint on
    twitter-server. DELETE requests with JSON bodies can be used to clear
    Tunables for a given id by making a request to /admin/tunables/$id.
    
    RB_ID=912969

--- src/main/scala/com/twitter/server/handler/TunableHandler.scala
+++ src/main/scala/com/twitter/server/handler/TunableHandler.scala
@@ -10,9 +10,9 @@ import com.twitter.util.{Throw, Return, Future}
 import com.twitter.util.tunable.{JsonTunableMapper, TunableMap}
 
 /**
- * In-memory Tunables can be manipulated using the endpoint `/admin/tunables/`.
- * To update the Tunables for a given id, make a PUT request to `/admin/tunables/$id`
- * with a JSON body in the same format as a Tunable configuration file:
+ * In-memory Tunables can be manipulated using the endpoint `/admin/tunables/`. PUT and DELETE
+ * requests to update the Tunables for a given id should be made to `/admin/tunables/$id`
+ * and have a JSON body in the same format as a Tunable configuration file:
  *
  * {
  *    "tunables":
@@ -30,8 +30,14 @@ import com.twitter.util.tunable.{JsonTunableMapper, TunableMap}
  *     ]
  * }
  *
- * These Tunables will be updated or added for the TunableMap corresponding to `id`. Note that
- * this PUT request will *not* cause any existing Tunables to be removed.
+ * In the case of a PUT, these Tunables will be updated or added for the TunableMap corresponding
+ * to `id`. Note that this PUT request will *not* cause any existing Tunables to be removed.
+ *
+ * In the case of a DELETE, these Tunables will cleared from the TunableMap corresponding
+ * to `id`. The Tunables are keyed by "id" and "type"; the "value" for each of Tunables to delete
+ * can be any valid value for this Tunable. Because the value of a Tunable is the result of a
+ * composition of TunableMaps (see [[StandardTunableMap]]), deleting an in-memory Tunable will cause
+ * the value from the composition of the other TunableMaps to be used.
  */
 class TunableHandler private[handler] (
     registeredIdsFn: () => Map[String, TunableMap])
@@ -81,14 +87,38 @@ class TunableHandler private[handler] (
       respond(Status.BadRequest, s"Expected Content-Type ${MediaType.Json} for PUT request")
   }
 
+  private[this] def handleDelete(req: Request): Future[Response] = req.contentType match {
+    case Some(MediaType.Json) =>
+      val json = req.contentString
+      JsonTunableMapper().parse(json) match {
+        case Return(tunableMap) =>
+          val id = req.path.stripPrefix(Path)
+          findMutable(registeredIdsFn(), id) match {
+            case None =>
+              respond(Status.NotFound, s"Mutable TunableMap not found for id: $id")
+            case Some(mutable) =>
+              mutable --= tunableMap
+              val successMsg = s"Successfully deleted tunables for id: $id"
+              log.info(successMsg)
+              respond(Status.Ok, successMsg)
+          }
+        case Throw(e) =>
+          respond(Status.BadRequest, s"Failed to parse JSON for DELETE request: ${e.getMessage}")
+      }
+    case unsupported =>
+      respond(Status.BadRequest, s"Expected Content-Type ${MediaType.Json} for DELETE request")
+  }
+
   def apply(req: Request): Future[Response] = req.method match {
     case Method.Put =>
       handlePut(req)
+    case Method.Delete =>
+      handleDelete(req)
     case unsupported =>
       respond(
         Status.MethodNotAllowed,
         s"Unsupported HTTP method: $unsupported",
-        Seq((Fields.Allow, "PUT")))
+        Seq((Fields.Allow, "PUT, DELETE")))
   }
 }
 
--- src/test/scala/com/twitter/server/handler/TunableHandlerTest.scala
+++ src/test/scala/com/twitter/server/handler/TunableHandlerTest.scala
@@ -8,39 +8,44 @@ import org.scalatest.FunSuite
 
 class TunableHandlerTest extends FunSuite {
 
-  test("PUT: Non-PUT request returns 'MethodNotAllowed response") {
+  test("Non-{DELETE, PUT} request returns 'MethodNotAllowed response") {
     val handler = new TunableHandler
     val resp = handler(Request(Method.Post, "/"))
     assert(Await.result(resp, 1.second).status == Status.MethodNotAllowed)
   }
 
-  test("PUT: Returns response with error if Media-Type is not JSON") {
-    val handler = new TunableHandler
-    val req = Request(Method.Put, "/")
-    req.contentType = MediaType.Csv
-    val resp = handler(req)
-    assert(Await.result(resp, 1.second).contentString.contains("Expected Content-Type"))
+  def testErrorHandling(method: Method) = {
+    test(method + ": Returns response with error if Media-Type is not JSON") {
+      val handler = new TunableHandler
+      val req = Request(method, "/")
+      req.contentType = MediaType.Csv
+      val resp = handler(req)
+      assert(Await.result(resp, 1.second).contentString.contains("Expected Content-Type"))
+    }
+
+    test(method + ": Returns response with error if JSON cannot be parsed") {
+      val handler = new TunableHandler
+      val req = Request(method, "/admin/tunables/foo")
+      req.contentType = MediaType.Json
+      req.contentString = "i am not valid json..."
+      val resp = handler(req)
+      assert(Await.result(resp, 1.second).contentString.contains("Failed to parse JSON"))
+    }
+
+    test(method + ": Returns response with Status.NotFound if id in path is not registered") {
+      val handler = new TunableHandler
+      val req = Request(method, "/admin/tunables/foo")
+      req.contentType = MediaType.Json
+      req.contentString = """{"tunables": [] }"""
+      val resp = Await.result(handler(req), 1.second)
+      assert(resp.contentString.contains(
+        "TunableMap not found for id: foo"))
+      assert(resp.status == Status.NotFound)
+    }
   }
 
-  test("PUT: Returns response with error if JSON cannot be parsed") {
-    val handler = new TunableHandler
-    val req = Request(Method.Put, "/admin/tunables/foo")
-    req.contentType = MediaType.Json
-    req.contentString = "i am not valid json..."
-    val resp = handler(req)
-    assert(Await.result(resp, 1.second).contentString.contains("Failed to parse JSON"))
-  }
-
-  test("PUT: Returns response with Status.NotFound if id in path is not registered") {
-    val handler = new TunableHandler
-    val req = Request(Method.Put, "/admin/tunables/foo")
-    req.contentType = MediaType.Json
-    req.contentString = """{"tunables": [] }"""
-    val resp = Await.result(handler(req), 1.second)
-    assert(resp.contentString.contains(
-      "Mutable TunableMap not found for id: foo"))
-    assert(resp.status == Status.NotFound)
-  }
+  testErrorHandling(Method.Put)
+  testErrorHandling(Method.Delete)
 
   test("PUT: Updates tunable map with new tunables") {
     val map = TunableMap.newMutable()
@@ -111,6 +116,56 @@ class TunableHandlerTest extends FunSuite {
     assert(map(key2)() == Some("i'd better stick around"))
     val resp = handler(req)
     assert(map(key1)() == Some("goodbye"))
+    assert(map(key2)() == Some("i'd better stick around"))
+    assert(Await.result(resp, 1.second).status == Status.Ok)
+  }
+
+  test("DELETE: Removes specified existing tunables") {
+    val map = TunableMap.newMutable()
+    val key = map.put("test_id", "hello")
+    val registry = Map[String, TunableMap]("foo" -> map)
+    val handler = new TunableHandler(() => registry)
+
+    val req = Request(Method.Delete, "/admin/tunables/foo")
+    req.contentType = MediaType.Json
+    req.contentString =
+      """{"tunables":
+        |  [
+        |     {
+        |         "id": "test_id",
+        |         "type": "java.lang.String",
+        |         "value": "remove me"
+        |     }
+        |  ]
+        |}""".stripMargin
+    val resp = handler(req)
+    assert(map(TunableMap.Key[String]("test_id"))() == None)
+    assert(Await.result(resp, 1.second).status == Status.Ok)
+  }
+
+  test("PUT: Does not remove existing tunables not present in update JSON") {
+    val map = TunableMap.newMutable()
+    val key1 = map.put("test_id1", "hello")
+    val key2 = map.put("test_id2", "i'd better stick around")
+    val registry = Map[String, TunableMap]("foo" -> map)
+    val handler = new TunableHandler(() => registry)
+
+    val req = Request(Method.Delete, "/admin/tunables/foo")
+    req.contentType = MediaType.Json
+    req.contentString =
+      """{"tunables":
+        |  [
+        |     {
+        |         "id": "test_id1",
+        |         "type": "java.lang.String",
+        |         "value": "remove me"
+        |     }
+        |  ]
+        |}""".stripMargin
+    assert(map(key1)() == Some("hello"))
+    assert(map(key2)() == Some("i'd better stick around"))
+    val resp = handler(req)
+    assert(map(key1)() == None)
     assert(map(key2)() == Some("i'd better stick around"))
     assert(Await.result(resp, 1.second).status == Status.Ok)
   }
