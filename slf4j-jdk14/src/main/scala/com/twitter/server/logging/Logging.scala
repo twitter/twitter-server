ec674bc5b970ffcb267f19b951e28a33ee70ba7c
Christopher Coco <ccoco@twitter.com>
2017-11-01 17:57:29 +0000

    twitter-server/slf4j-jdk14: Apply log format in trait constructor
    Summary: Problem
    
    The application of the LogFormat to the JUL logging handlers
    is done in the `premain` lifecycle of the server which is
    after a fair amount of server constructor logging.
    
    Solution
    
    Move the application of the LogFormat to the constructor
    of the trait which will apply the LogFormat to the configured
    handlers earlier. This comes with the drawbacks that anyone
    overrriding the log format in the trait will now not be able
    to use flags for configuration.
    
    In practice this is most likely not a general concern.
    
    Differential Revision: https://phabricator.twitter.biz/D106534

--- slf4j-jdk14/src/main/scala/com/twitter/server/logging/Logging.scala
+++ slf4j-jdk14/src/main/scala/com/twitter/server/logging/Logging.scala
@@ -6,9 +6,22 @@ import java.util.logging.Logger
 
 trait Logging extends ctl.Logging { self: App =>
 
-  premain {
+  /**
+   * Note: we are applying the `defaultFormatter` to any
+   * configured handlers on the ROOT logger in the constructor
+   * to apply the desired formatting as early as possible to
+   * logged statements.
+   *
+   * A consequence of this is that overrides of the `defaultFormatter`
+   * cannot use flags for configuration of a formatter as flags are
+   * not parsed until later in the App lifecycle and thus would only
+   * have their default values (if any) when the `defaultFormatter`
+   * function is called here.
+   */
+  {
+    val formatter = defaultFormatter
     for (h <- Logger.getLogger("").getHandlers)
-      h.setFormatter(defaultFormatter)
+      h.setFormatter(formatter)
   }
 
   override def defaultFormatter: ctl.Formatter = new LogFormatter
