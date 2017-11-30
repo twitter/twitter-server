233d30a38605b62304e92f3d57918ee40058479f
Isabel Martin <imartin@twitter.com>
2017-11-30 00:27:39 +0000

    twitter-server: Change log4j log levels without configuration changes
    Summary: Problem:
    
    Users would like to dynamically change logging levels in the
    admin interface without having to configure those loggers
    beforehand.
    
    Solution:
    
    Display all loggers in interface regardless of whether it
    has been configured or not. This change includes a way to
    filter which loggers are displayed, and a way to reset them
    after they've been changed dynamically.
    
    JIRA Issues: CSL-5523
    
    Differential Revision: https://phabricator.twitter.biz/D115140

--- slf4j-log4j12/src/main/scala/com/twitter/server/handler/slf4j/log4j12/LoggingHandler.scala
+++ slf4j-log4j12/src/main/scala/com/twitter/server/handler/slf4j/log4j12/LoggingHandler.scala
@@ -28,7 +28,8 @@ private class LoggingHandler extends AdminHttpMuxHandler with Logging {
     Level.ALL
   ).sorted(levelOrder)
 
-  private[this] val loggers = {
+  /** Exposed for testing */
+  private[log4j12] def loggers = {
     LogManager.getCurrentLoggers.asScala.toSeq.asInstanceOf[Seq[Logger]]
   }
 
@@ -68,6 +69,12 @@ private class LoggingHandler extends AdminHttpMuxHandler with Logging {
               throw new IllegalArgumentException(
                 s"Unable to set log level for $loggerString -- undefined logging level!"
               )
+            } else if (toSetLevel == "null") {
+              val logger = LogManager.getLogger(loggerString)
+              logger.setLevel(null)
+              val message = s"Removed level override for ${logger.getName}"
+              info(message)
+              escapeHtml(message)
             } else {
               val logger = LogManager.getLogger(loggerString)
               logger.setLevel(Level.toLevel(toSetLevel))
@@ -83,9 +90,17 @@ private class LoggingHandler extends AdminHttpMuxHandler with Logging {
         case _ => ""
       }
 
+      val showOverriddenOnly = request.getBooleanParam("overridden", false)
+      val filteredLoggers = if (showOverriddenOnly) {
+        loggers
+          .filter(_.getLevel != null)
+          .filter(_.getName != org.slf4j.Logger.ROOT_LOGGER_NAME)
+      } else loggers
+      val html = renderHtml(filteredLoggers, levels, message, showOverriddenOnly)
+
       newResponse(
         contentType = "text/html;charset=UTF-8",
-        content = Buf.Utf8(renderHtml(loggers, levels, message))
+        content = Buf.Utf8(html)
       )
     } catch {
       case e: Throwable =>
@@ -93,7 +108,11 @@ private class LoggingHandler extends AdminHttpMuxHandler with Logging {
     }
   }
 
-  private def renderHtml(loggers: Seq[Logger], levels: Seq[Level], updateMsg: String): String =
+  private def renderHtml(
+    loggerList: Seq[Logger],
+    levels: Seq[Level], updateMsg:
+    String, showOverriddenOnly: Boolean
+  ): String =
     s"""<table class="table table-striped table-condensed">
         <caption>${escapeHtml(updateMsg)}</caption>
         <thead>
@@ -102,29 +121,46 @@ private class LoggingHandler extends AdminHttpMuxHandler with Logging {
             <th>org.apache.log4j.Level</th>
           </tr>
         </thead>
-        ${(for (logger <- loggers) yield {
+        ${renderButtons(showOverriddenOnly)}
+        ${(for (logger <- loggerList) yield {
       val loggerName =
         if (logger.getName == "") org.slf4j.Logger.ROOT_LOGGER_NAME else logger.getName
-      s"""<tr>
-                <td><h5>${escapeHtml(loggerName)}</h5></td>
-                <td><div class="btn-group" role="group">
-                  ${(for (level <- levels) yield {
+      val inheritsLevel = logger.getLevel == null
+      val buttons = for (level <- levels) yield {
         val isActive = logger.getEffectiveLevel == level
-        val activeCss =
-          if (!isActive) "btn-default"
-          else {
-            "btn-primary active disabled"
-          }
+        val activeCss = if (!isActive) "btn-default"
+          else if (!inheritsLevel) "btn-primary active disabled"
+          else "btn-primary active"
         val href =
-          if (isActive) ""
+          if (isActive && logger.getLevel == logger.getEffectiveLevel) ""
           else {
             s"""?logger=${URLEncoder.encode(loggerName, "UTF-8")}&level=${level.toString}"""
           }
         s"""<a class="btn btn-sm $activeCss"
                               href="$href">${level.toString}</a>"""
-      }).mkString("\n")}
-                </div></td>
-                </tr>"""
+      }
+      val resetButton = if (!inheritsLevel && loggerName != org.slf4j.Logger.ROOT_LOGGER_NAME) {
+        val href = s"""?logger=${URLEncoder.encode(loggerName, "UTF-8")}&level=null"""
+        s"""<a class="btn btn-sm btn-warning" href="$href">RESET</a>"""
+      } else ""
+      s"""<tr>
+        <td><h5>${escapeHtml(loggerName)}</h5></td>
+          <td><div class="btn-group" role="group">
+            ${buttons.mkString("\n")}
+            $resetButton
+          </div></td>
+          </tr>"""
     }).mkString("\n")}
          </table>"""
+
+  private def renderButtons(showOverriddenOnly: Boolean): String = {
+    def buttonClass(inject: String): String = s"""class="btn btn-primary$inject""""
+    val disabledButton = buttonClass(""" active disabled""")
+    val overrideButton = buttonClass("""" href="?overridden=true""")
+    val (showAllBtn, overriddenBtn) =
+      if (showOverriddenOnly) (buttonClass("""" href="?"""), disabledButton)
+      else (disabledButton, overrideButton)
+    s"""<a $showAllBtn>Show all loggers</a>
+        <a $overriddenBtn>Show overridden loggers only</a>"""
+  }
 }
---
+++ slf4j-log4j12/src/test/scala/com/twitter/server/handler/slf4j/log4j12/LoggingHandlerTest.scala
@@ -0,0 +1,72 @@
+package com.twitter.server.handler.slf4j.log4j12
+
+import com.twitter.conversions.time._
+import com.twitter.finagle.http.Request
+import com.twitter.util.Await
+import com.twitter.util.logging.{Logger => UtilLogger}
+
+import org.apache.log4j.{Level, LogManager}
+import org.scalatest.FunSuite
+import org.slf4j.LoggerFactory
+
+class LoggingHandlerTest extends FunSuite {
+
+  private val handler = new LoggingHandler()
+
+  test("sees logger created by implicit ClassTag") {
+    class ClassTagLogger()
+
+    val log = UtilLogger[ClassTagLogger]
+
+    assert(handler.loggers.map(_.getName).contains(classOf[ClassTagLogger].getName))
+  }
+
+  test("sees logger created for the given name") {
+    val log = UtilLogger("name")
+
+    assert(handler.loggers.map(_.getName).contains("name"))
+  }
+
+  test("sees logger created for given class") {
+    class SomeClass()
+
+    val log = UtilLogger(classOf[SomeClass])
+
+    assert(handler.loggers.map(_.getName).contains(classOf[SomeClass].getName))
+  }
+
+  test("sees logger created to wrap an underlying org.slf4j.Logger") {
+    val underlying = LoggerFactory.getLogger("name")
+    val log = UtilLogger(underlying)
+
+    assert(handler.loggers.map(_.getName).contains(underlying.getName))
+  }
+
+  test("can set logging levels for logger created by implicit ClassTag") {
+    class ClassTagLogger()
+
+    val log = UtilLogger[ClassTagLogger]
+
+    val loggerName = classOf[ClassTagLogger].getName
+    val logger = LogManager.getLogger(loggerName)
+
+    logger.setLevel(Level.WARN)
+    assert(handler.loggers.filter(_.getName == loggerName).head.getLevel() == Level.WARN)
+
+    logger.setLevel(Level.DEBUG)
+    assert(handler.loggers.filter(_.getName == loggerName).head.getLevel() == Level.DEBUG)
+  }
+
+  test("override can be reset") {
+    val req = Request(("logger", "foo"), ("level", "null"))
+    val logger = LogManager.getLogger("foo")
+
+    assert(logger.getLevel == null)
+
+    logger.setLevel(Level.DEBUG)
+    Await.result(handler(req), 5.seconds)
+
+    assert(logger.getLevel == null)
+  }
+
+}
