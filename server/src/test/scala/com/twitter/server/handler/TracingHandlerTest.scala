2b2140c617f90e8e100a8d45ac00637de86c0135
Vladimir Kostyukov <vkostyukov@twitter.com>
2018-07-19 03:57:43 +0000

    finagle-core: Call Trace.isActivelyTracing less
    # Problem
    
    We call `isActivelyTracing` too often to prevent unnecessarily structures from materializing (a very simple
    proxy reports 12 calls/request). The overhead of `isActivelyTracing` (about 5% of CPU profile) may outweigh
    the benefits of saving allocations.
    
    Additionally, a canonical usage pattern requires users to guard tracing operations behind the `isActivelyTracing`
    result, which pretty much doubles the number of checks we do today.
    
    # Solution
    
    Do not check `isActivelyTracing` internally before materializing annotations and rely on callers checking
    that externally. This cuts the number of `isActivelyTracing` calls in a proxy in half: from 12 to 7.
    
    JIRA Issues: CSL-5625
    
    Differential Revision: https://phabricator.twitter.biz/D193300

--- server/src/test/scala/com/twitter/server/handler/TracingHandlerTest.scala
+++ server/src/test/scala/com/twitter/server/handler/TracingHandlerTest.scala
@@ -1,11 +1,9 @@
 package com.twitter.server.handler
 
 import com.twitter.finagle.http.{Request, Status}
-import com.twitter.finagle.tracing.{Record, Trace, TraceId, Tracer}
+import com.twitter.finagle.tracing.{Trace, Tracer}
 import com.twitter.util.Await
 import org.junit.runner.RunWith
-import org.mockito.Matchers.any
-import org.mockito.Mockito.{never, verify, when}
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{BeforeAndAfter, FunSuite}
@@ -16,20 +14,13 @@ class TracingHandlerTest extends FunSuite with MockitoSugar with BeforeAndAfter 
 
   test("enable tracing") {
     val tracer = mock[Tracer]
-    when(tracer.isActivelyTracing(any[TraceId])).thenReturn(true)
-
     try {
-      // FIXME: This relies on a global.
       Trace.disable()
       Trace.letTracer(tracer) {
-        Trace.record("msg")
-        verify(tracer, never()).record(any(classOf[Record]))
-
+        assert(!Trace.enabled)
         val request = Request("/", ("enable", "true"))
         assert(Await.result(service(request)).status == Status.Ok)
-
-        Trace.record("msg")
-        verify(tracer).record(any(classOf[Record]))
+        assert(Trace.enabled)
       }
     } finally {
       Trace.enable()
@@ -38,22 +29,13 @@ class TracingHandlerTest extends FunSuite with MockitoSugar with BeforeAndAfter 
 
   test("disable tracing") {
     val tracer = mock[Tracer]
-    when(tracer.isActivelyTracing(any[TraceId])).thenReturn(true)
-
     try {
       Trace.enable()
       Trace.letTracer(tracer) {
-        Trace.record("msg")
-        verify(tracer).record(any(classOf[Record]))
-
-        val tracer2 = mock[Tracer]
-        Trace.letTracer(tracer2) {
-          val request = Request("/", ("disable", "true"))
-          assert(Await.result(service(request)).status == Status.Ok)
-
-          Trace.record("msg")
-          verify(tracer2, never()).record(any(classOf[Record]))
-        }
+        assert(Trace.enabled)
+        val request = Request("/", ("disable", "true"))
+        assert(Await.result(service(request)).status == Status.Ok)
+        assert(!Trace.enabled)
       }
     } finally {
       Trace.enable()
