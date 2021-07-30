d6f66d78be00dc946b3e0a2fd79d5d3275a77d7f
Moses Nakamura <mnakamura@twitter.com>
2021-07-30 22:36:15 +0000

    twitter-server: Updated the "too many statsreceivers" lint error to recommend loadServiceDenied
    Problem
    
    The lint error recommends using loadServiceIgnoredPaths which is not very
    fashionable.
    
    Solution
    
    Instead recommend loadServiceDenied, which is what the cool kids use!!
    
    JIRA Issues: CSL-11218
    
    Differential Revision: https://phabricator.twitter.biz/D714715

--- server/src/main/scala/com/twitter/server/lint/NumberOfStatsReceiversRule.scala
+++ server/src/main/scala/com/twitter/server/lint/NumberOfStatsReceiversRule.scala
@@ -16,7 +16,7 @@ object NumberOfStatsReceiversRule {
       "More than one StatsReceiver loaded causes a larger than necessary " +
         "memory footprint and slower runtime usage. Examine your (transitive) " +
         "dependencies and remove unwanted StatsReceivers either via dependency " +
-        "management or the com.twitter.finagle.util.loadServiceIgnoredPaths flag. " +
+        "management or the com.twitter.finagle.util.loadServiceDenied flag. " +
         "Alternatively, having none loaded indicates that the service will not " +
         "have any telemetry reported which is dangerous way to operate a service."
     ) {
