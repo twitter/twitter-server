b52d9b343abf73a8f84f157cda8d6fd21f0efb77
Ian Bennett <ibennett@twitter.com>
2021-08-13 21:02:08 +0000

    twitter-server: add test coverage to ShutdownHandlerTest
    Problem/Solution
    
    For the `ShutdownHandlerTest` we do not have coverage
    for the case where the `defaultCloseGracePeriod` is
    overridden. Let's modify tests to allow for testing
    and verifying this behavior.
    
    JIRA Issues: CSL-11231
    
    Differential Revision: https://phabricator.twitter.biz/D717003

--- server/src/test/scala/com/twitter/server/handler/ShutdownHandlerTest.scala
+++ server/src/test/scala/com/twitter/server/handler/ShutdownHandlerTest.scala
@@ -1,9 +1,9 @@
 package com.twitter.server.handler
 
 import com.twitter.conversions.DurationOps._
-import com.twitter.finagle.http.{Status, Request, Method}
+import com.twitter.finagle.http.{Method, Request, Status}
 import com.twitter.server.TwitterServer
-import com.twitter.util.{Await, Awaitable, Closable, Future, Time}
+import com.twitter.util.{Await, Awaitable, Closable, Duration, Future, Time}
 import org.scalatest.funsuite.AnyFunSuite
 
 class ShutdownHandlerTest extends AnyFunSuite {
@@ -11,6 +11,10 @@ class ShutdownHandlerTest extends AnyFunSuite {
   private[this] def await[T](a: Awaitable[T]): T = Await.result(a, 2.seconds)
 
   class Closer(testDeadline: Time => Unit) extends TwitterServer {
+
+    @volatile var defaultGraceOverride = Duration.Zero
+    override def defaultCloseGracePeriod: Duration = defaultGraceOverride
+
     @volatile var closed = false
     closeOnExit {
       Closable.make { deadline =>
@@ -36,6 +40,21 @@ class ShutdownHandlerTest extends AnyFunSuite {
     assert(rsp.status == Status.Ok)
     assert(closer.closed)
   })
+
+  test("close without a grace period, but closer overrides default grace")(
+    Time.withCurrentTimeFrozen { tc =>
+      val grace = 5.seconds
+      val now = Time.now
+      val closer = Closer.mk { deadline =>
+        // MinGrace is 1 second
+        assert(deadline == now + grace)
+      }
+      closer.defaultGraceOverride = grace
+      val handler = new ShutdownHandler(closer)
+      val rsp = await(handler(Request(Method.Post, "/foo")))
+      assert(rsp.status == Status.Ok)
+      assert(closer.closed)
+    })
 
   test("close with a grace period") {
     val grace = 10.seconds
