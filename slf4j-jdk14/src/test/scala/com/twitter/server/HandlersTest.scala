2db9bf6eff8091305d42302ebd8c25b605917fb9
Bryce Anderson <banderson@twitter.com>
2022-11-04 01:26:17 +0000

    finagle, twitter-server: Fix some tests that are flaky with OffloadFilter enabled
    Problem
    
    Some tests have faulty assumptions about event ordering that breaks down
    with OffloadFilter enabled. That is preventing us from changing the
    default behavior.
    
    Solution
    
    Fix the tests.
    
    Differential Revision: https://phabricator.twitter.biz/D1011251

--- slf4j-jdk14/src/test/scala/com/twitter/server/HandlersTest.scala
+++ slf4j-jdk14/src/test/scala/com/twitter/server/HandlersTest.scala
@@ -1,11 +1,17 @@
 package com.twitter.server
 
-import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finagle.{Http, Service}
-import com.twitter.util.{Await, Future}
+import com.twitter.finagle.filter.OffloadFilter
+import com.twitter.finagle.http.Request
+import com.twitter.finagle.http.Response
+import com.twitter.finagle.Http
+import com.twitter.finagle.Service
+import com.twitter.util.Await
+import com.twitter.util.Future
 import java.io.ByteArrayOutputStream
 import java.net.InetSocketAddress
-import java.util.logging.{Logger, SimpleFormatter, StreamHandler}
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
+import java.util.logging.StreamHandler
 import scala.collection.mutable
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -48,6 +54,11 @@ class HandlersTest extends AnyFunSuite {
   test("Exceptions thrown in handlers include stack traces") {
     val twitterServer: TwitterServer = new TestTwitterServer {
       val mockExceptionHandler = new MockExceptionHandler
+
+      override protected def configureAdminHttpServer(server: Http.Server): Http.Server = {
+        // TODO: with offload filter there appears to be a race condition in the test.
+        super.configureAdminHttpServer(server).configured(OffloadFilter.Param.Disabled)
+      }
 
       override def main(): Unit = {
         addAdminRoute(
